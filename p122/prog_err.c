0ee3ecd464f4bf47ea4fd5ca0cc0097048e065ca
Eugeny <m01ves2@yandex.ru>
2017-08-21 15:35:13 +0300

    first c program added

---
+++ p122/prog_err.c
@@ -0,0 +1,11 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+	fprintf(stderr, "ENOMEM: %s\n", strerror(ENOMEM));
+	errno = ENOEXEC;
+	perror(argv[0]);
+	return 0;
+}
